9d3e6cf4d8d41c3a881a7a9fd03b54aa194cfe3c
Kyle Phillips <kylep@Kyles-imac.fios-router.home>
2019-02-25 13:51:27 -0500

    Implemented changes from feedback restructured ThemPark function in typescript

--- app.ts
+++ app.ts
@@ -14,13 +14,13 @@ if (nameA.charCodeAt(0) >= 77) {
     alert('Next!');
 }
 // sayHello function
-function sayHello(Phone: any) {
+function sayHello(Phone: string) {
     alert(Phone);
 }
 sayHello('Samsung');
 sayHello('Apple');
 
-function add(water:any, juice:any) {
+function add(water: number, juice: number) {
     (water + juice)
     return name
 }
@@ -32,7 +32,7 @@ console.log(myNum);
 
 let names = ['Charles', 'Abby', 'James', 'John'];
 
-function checkMyAge(name:any, age:any):void  {
+function checkMyAge(name: string, age: number): void {
     if (age < 21) {
         alert(`Sorry ${name} you aren't old enough to view this page!`);
     } else {
@@ -58,32 +58,36 @@ for (let i = 0; i < favVeggies.length; i++) {
 }
 
 // Create an array of 5 objects that contain name and age properties.
+interface ThemePark {
+    name: string;
+    age: number;
+}
+let Person1: ThemePark = {
+    name: 'Clyde',
+    age: 23
+};
+let Person2: ThemePark = {
+    name: 'Aaron',
+    age: 14
+}
 
-let ThemePark = [{
-        name: 'Clyde',
-        age: 23
-    },
-    {
-        name: 'Aaron',
-        age: 14
-    },
-    {
-        name: 'Ashley',
-        age: 19
-    },
-    {
-        name: 'Vincent',
-        age: 34
-    },
-    {
-        name: 'Ryan',
-        age: 26
-    }
-];
+let Person3: ThemePark = {
+    name: 'Ashley',
+    age: 19
+}
+
+let Person4: ThemePark = {
+    name: 'Vincent',
+    age: 34
+}
+let Person5: ThemePark = {
+    name: 'Ryan',
+    age: 26
+};
 
 // Use a loop to call the checkAge function for each object in the array, passing the object's name and age as arguments.
 
-function checkAge(name:any, age:any) {
+function checkAge(name: string, age: number) {
 
     if (age < 21) {
         alert(`Sorry + ${name},${age} you STILL aren't old enough to view this page!`);
@@ -91,31 +95,31 @@ function checkAge(name:any, age:any) {
         alert('WELCOME TO BIKINI BOTTOM');
     }
 }
+let nameAge: Array<ThemePark> = [Person1,Person2,Person3, Person4, Person5]
+for (let i: number = 0; i<nameAge.length; i++) {
+    checkAge(nameAge[i].name, nameAge[i].age);
+}
 
-checkAge(ThemePark[0], 23);
-checkAge(ThemePark[1], 14);
-checkAge(ThemePark[2], 19);
-checkAge(ThemePark[3], 34);
-checkAge(ThemePark[4], 26);
+
 
 
 // getLength that takes any word as an argument
-function getLength(greeting:any)    {
-   
-    return(greeting.length)
+function getLength(greeting: any) {
+
+    return (greeting.length)
 }
 
 getLength("HelloWorld");
 // Store the returned result of that function in a variable.
 // Check the number in the variable from the previous objective. 
 // Find out if it;s odd or even
-function getGreetLength(words: string)   {
-    if(words.length %2 ==0) {
+function getGreetLength(words: string) {
+    if (words.length % 2 == 0) {
         alert(`The World Is Nice & Even`);
-    }   else   {
+    } else {
         alert(`The World Is An Odd Place`);
 
     }
-  console.log(words.length)
+    console.log(words.length)
 }
 console.log(getGreetLength('hi mom'))
